5ed514a0829be2336f4021403a6a299f0cac6c9a
Science6uru <andrew.s.owen@icloud.com>
2025-07-10 04:50:13 +0000

    adding boilerplate setup

---
+++ src/admin_manage.php
@@ -0,0 +1,7 @@
+This is the <b>manage</b> Page for staiton <?=$station_number ?>
+<br/>
+<?php require plugin_dir_path( __FILE__ ) ."carousel.php" ?>
+
+<?php 
+	print "fpp db version is" . get_option( 'fpp_db_version' );
+?>
---
+++ src/plugin-admin.php
@@ -0,0 +1,55 @@
+<?php
+add_action( 'admin_menu', 'fpp_admin_register' );
+
+function fpp_admin_register()
+{
+    add_menu_page(
+        'FPP Plugin Admin',     // page title
+        'FPP Admin',     // menu title
+        'manage_options',   // capability
+        'fpp-plugin-admin',     // menu slug
+        'fpp_admin_render' // callback function
+    );
+    add_submenu_page("fpp-plugin-admin", "Manage Station 1", "Station 1", "edit_posts", "fpp-plugin-manage-1", "fpp_admin_manage_render");
+    add_submenu_page("fpp-plugin-admin", "Manage Station 2", "Station 2", "edit_posts", "fpp-plugin-manage-2", "fpp_admin_manage_render");
+}
+function fpp_admin_render()
+{
+    global $title;
+
+    print '<div class="wrap">';
+    print "<h1>$title</h1>";
+
+    $file = plugin_dir_path( __FILE__ ) . "admin_dashboard.php";
+
+    if ( file_exists( $file ) )
+        require $file;
+    else
+        print "File Not Found";
+
+    //print "<p class='description'>Included from <code>$file</code></p>";
+
+    print '</div>';
+}
+
+function fpp_admin_manage_render()
+{
+    global $title;
+    $page_slug = $_GET['page'];
+    $parts = explode('-', $page_slug);
+    $station_number = end($parts);
+
+    print '<div class="wrap">';
+    print "<h1>$title</h1>";
+    $file = plugin_dir_path( __FILE__ ) . "admin_manage.php";
+
+    if ( file_exists( $file ) )
+        require $file;
+    else
+        print "File Not Found";
+
+    //print "<p class='description'>Included from <code>$file</code></p>";
+
+    print '</div>';
+}
+?>
