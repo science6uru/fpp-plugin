0c2a25933640ae95acaa6a9cfbf1b6e1eef943f4
thinhcomepan <thinhphanpro151007@gmail.com>
2025-09-01 22:14:21 +0000

    test commit

--- src/fpp-plugin-fpp_upload.php
+++ src/fpp-plugin-fpp_upload.php
@@ -2,7 +2,7 @@ This is the fpp_upload page <?=$station_id?>
 
 <form action="/?rest_route=/fpp/v1/photo_upload/<?=$station_id?>" method="post" enctype="multipart/form-data">
   <label for="file-upload">Choose a file to upload:</label>
-  <input type="text" id="file-upload" name="user_photo" required>
+  <input type="file" id="file-upload" name="user_photo" required>
   <button type="submit">Upload File</button>
 </form>
 
